ccd0a37cfb0b3109394a5c32533fdfcc83e807e9
Mehmet <you@example.com>
2025-11-13 12:43:57 +0100

    41_If-Aufgaben_Else Done

---
+++ 40_If-Bedingungen_Basics/41_If-Aufgaben_Else.js
@@ -0,0 +1,15 @@
+let a = -8
+
+// a > 10 größer
+// a < 10 kleiner
+// a == 7
+
+// a >= 10 größer gleich 
+// a <= 10 kleiner gleich
+
+if (a >= 10) {
+// Blockklammern
+    console.log("Die Zahl ist groß")
+} else {
+    console.log("Die Zahl ist klein")
+}

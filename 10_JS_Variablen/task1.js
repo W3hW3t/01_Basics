27af2c461f1ade078b147211f705de15101e8fd8
Mehmet <you@example.com>
2025-12-04 12:43:46 +0100

    10 Task1 Done

---
+++ 10_JS_Variablen/task1.js
@@ -0,0 +1,23 @@
+console.log("Hallo"); 
+
+console.log("Tolle Sache"); 
+
+let a = 7; 
+let b = 8; 
+let c = 10; 
+
+let res1 = a + b + c;
+
+console.log("a + b + c =", res1);
+console.log("(a + b) / c =", (a + b) / c);
+console.log("c - a + b =", c - a + b);
+console.log("a + b + c * 3 =", a + b + c * 3);
+console.log("a + b + c) / 4 =", (a + b + c) / 4);
+
+a = 3 
+b = a + 3 
+c = c - 3 
+
+console.log("Neuer Wert von a =", a);
+console.log("Neuer Wert von b =", b);
+console.log("Neuer Wert von c =", c);

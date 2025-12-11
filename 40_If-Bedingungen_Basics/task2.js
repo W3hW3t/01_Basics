d103dd36ded9080591bda983a2e8393dae373677
Mehmet <you@example.com>
2025-12-11 13:01:30 +0100

    40 Task2 Done

---
+++ 40_If-Bedingungen_Basics/task2.js
@@ -0,0 +1,13 @@
+let trafficlight = "red";
+
+if (trafficlight === "green") {
+  console.log("You may go!");
+} 
+
+else if (trafficlight === "yellow") {
+  console.log("Caution!");
+} 
+
+else if (trafficlight === "red") {
+  console.log("Stop!");
+}

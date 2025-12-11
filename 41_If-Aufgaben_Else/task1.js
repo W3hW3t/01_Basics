7dc5dea7dac68a6d6628556d58d61c8f4441b76e
Mehmet <you@example.com>
2025-12-11 13:07:06 +0100

    41 Task1 Done

---
+++ 41_If-Aufgaben_Else/task1.js
@@ -0,0 +1,11 @@
+let a = 0
+
+if (a > 0) {
+    console.log("The Number is positive")
+} else if (a < 0) {
+    console.log("The Number is negative")
+} else if (a == 0) {
+    console.log("The Number is zero")
+} else {
+    console.log("The Number is not 0")
+}

5c859585047592ed7e659eeb9e83a70ae28faa42
Mehmet <you@example.com>
2025-12-18 12:10:33 +0100

    42 Task1 Done

---
+++ 42_If-Bedingungen if-else/task1.js
@@ -0,0 +1,9 @@
+let a = Math.floor(Math.random() * 10); 
+
+if (a < 5) {
+    console.log("The number is less than 5" )
+} else if (a > 5) {
+    console.log("The number is greater than 5" )
+} else if (a = 5) {
+    console.log("The number is exactly 5" )
+}

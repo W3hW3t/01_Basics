4d87df290c49b6791eae685e161573f0b568114c
Mehmet <you@example.com>
2026-01-08 12:50:03 +0100

    45 Task 2 Done

---
+++ 45_Deno/task2.js
@@ -0,0 +1,17 @@
+let a = 12;
+
+if (a > 0) {
+console.log("The Number is positive");
+} else {
+console.log("The Number is not positive");
+}
+if (a < 0) {
+console.log("The Number is negative");
+} else {
+console.log("The Number is positiv or zero");
+}
+if (a == 0) {
+console.log("The Number is zero");
+} else {
+console.log("The Number is not zero");
+}
--- VSC/demo.js
+++ VSC/demo.js
@@ -4,4 +4,4 @@ console.log("Hello");
 // PS C:\Users\mehme\Documents\Schule\2026\SWP\01_Basics> cd .\[Folder Name]\
 // PS C:\Users\mehme\Documents\Schule\2026\SWP\01_Basics\[Folder Name]> ls
 // PS C:\Users\mehme\Documents\Schule\2026\SWP\01_Basics\[Folder Name]> deno .\[File Name].js
-// ctrl + s to save
+// ctrl + s to save file and rerun the command to see changes

73c4eee667160364eda892149833eb28983c6684
Mehmet <you@example.com>
2026-01-15 12:49:28 +0100

    Fixed  ton of Tasks to be more simple

--- 45_Deno/task5.js
+++ 45_Deno/task5.js
@@ -1,18 +1,13 @@
-const eingabe = prompt("Wie viel Grad hat es gerade (in Celsius)?");
-const temperatur = Number(eingabe);
+let temp = prompt("Wie viel Grad hat es?");
 
-// 2. Logik zur Empfehlung
-if (isNaN(temperatur)) {
-    console.log("Bitte gib eine gültige Zahl für die Temperatur ein.");
-} else if (temperatur < 0) {
-    console.log("Es ist eiskalt! Drinnen bleiben.");
-} else if (temperatur < 15) {
-    console.log("Ziemlich kühl. Warm anziehen!");
-} else if (temperatur < 25) {
-    console.log("Das Wetter ist super. Raus gehen!");
-} else if (temperatur < 35) {
-    console.log("Es ist heiß. Baden gehen!");
+if (temp < 0) {
+    console.log("Drinnen bleiben");
+} else if (temp < 15) {
+    console.log("Warm anziehen");
+} else if (temp < 25) {
+    console.log("Raus gehen");
+} else if (temp < 35) {
+    console.log("Baden gehen");
 } else {
-    // Alles über 35 Grad
-    console.log("Viel zu heiß! Ab in die Kühlbox!");
+    console.log("In die Kühlbox gehen");
 }

be019a5ce4c1f84ccbe1370e1519f66ba8e3e650
Mehmet <you@example.com>
2026-01-22 12:26:02 +0100

    11 Task 1 Done

---
+++ 11_Arrays/task1.js
@@ -0,0 +1,8 @@
+let arr = [12, 5, 22, 9, 30, 2, 15];
+let summearr = 0;
+
+for (let i = 0; i < arr.length; i = i + 1) {
+    summearr = summearr + arr[i];
+}
+
+console.log("Die Summe der arr ist: " + summearr);
